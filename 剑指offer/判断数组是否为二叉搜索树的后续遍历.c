f0f9ed9dabf0c895cf46fdb2d93fbc3e2db47f7f
朱亚光 <zhuyaguang1368@163.com>
2016-07-02 19:47:57 +0800

    7.2 题

---
+++ 剑指offer/判断数组是否为二叉搜索树的后续遍历.c
@@ -0,0 +1,25 @@
+bool judge(int a[],int l,int r){
+	if (l>=r)
+	{
+		return true;
+	}
+
+	int i = r;
+	for (i>1 && a[i-1] > a[r]) --i;
+
+		for (int j = i-1; j >1; j--)
+		{
+			if (a[j] > a[r])
+			{
+				return false;
+			}
+
+			return judge(a,l,i-1) && judge(a,i,r-1);
+		}
+}
+
+ bool VerifySquenceOfBST(int a[]) {
+        if(!a.size()) return false;
+
+        return judge(a, 0, a.size() - 1);
+   }
