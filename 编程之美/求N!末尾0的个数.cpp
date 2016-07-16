174d8d58777897b359cedf56f9c7b4114de04279
朱亚光 <zhuyaguang@users.noreply.github.com>
2016-07-16 08:45:30 +0800

    7.15

--- 编程之美/求N!末尾0的个数.cpp
+++ 编程之美/求N!末尾0的个数.cpp
@@ -19,3 +19,37 @@ void main()
 	cout<<tail_num_0(10)<<endl;
 	system("pause");
 }
+
+//
+#include<iostream>
+using namespace std;
+
+int numofzero(int num)
+{
+    int count = 0;
+    while (num) {
+        if (num % 5 ==0) {
+            count++;
+            
+        }
+        num = num /5 ;
+    }
+    
+    return count;
+}
+
+int jie(int num)
+{
+    if (num == 0) {
+        return 1;
+    }
+    return num * jie(num -1 );
+}
+int main(){
+   
+    int num;
+    cin>>num;
+    
+    cout<<numofzero(jie(num));
+
+}
